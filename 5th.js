41661fd0641c58d5a99d0ed82c4a421c1bdbf0bd
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-14 05:32:33 +0600

    vulbaal

--- 5th.js
+++ 5th.js
@@ -8,11 +8,11 @@ for (const word of words) {
 console.log(str);
 // 2nd method
 let str2 = "";
-for(i = 0; i < words.length; i++) {
+for (i = 0; i < words.length; i++) {
     const word = words[i];
     str2 = word + str2;
 }
-console.log(str2)
+console.log(str2);
 // 3rd method
 const newStatement = statement.split("").reverse().join("");
 console.log(newStatement);
